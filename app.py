a612c61a3f1573f4a2c583c2067bb3687938389a
NickBeckley23 <40444243+NickBeckley23@users.noreply.github.com>
2018-06-21 00:55:15 -0600

    Formatting
    prettified html and python

--- app.py
+++ app.py
@@ -1,58 +1,66 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import requests
 from flask import Flask, render_template, request
 
 URL = 'https://api.fortnitetracker.com/v1/profile/pc/{}'
-headers = {'TRN-Api-Key' : '082225fc-cf28-4107-9a3e-c7263cd0a22c'}
+headers = {'TRN-Api-Key': '082225fc-cf28-4107-9a3e-c7263cd0a22c'}
 
-#res = requests.get(URL, headers=headers)
-#result = res.json()['lifeTimeStats']
+# res = requests.get(URL, headers=headers)
+# result = res.json()['lifeTimeStats']
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	player_one = None
-	player_two = None
-	player_one_stats = {}
-	player_two_stats = {}
-	
-	if request.method == 'POST':
-		player_one = request.form.get('playerOneName')
+    player_one = None
+    player_two = None
+    player_one_stats = {}
+    player_two_stats = {}
 
-		if player_one:
-			player_two = request.form.get('playerName')
-		else:
-			player_one = request.form.get('playerName')
+    if request.method == 'POST':
+        player_one = request.form.get('playerOneName')
 
-		player_one_result = requests.get(URL.format(player_one), headers=headers).json()['lifeTimeStats']
-		player_one_stats = populate_player_data(player_one_result)
+        if player_one:
+            player_two = request.form.get('playerName')
+        else:
+            player_one = request.form.get('playerName')
 
-		if player_two:
-			player_two_result = requests.get(URL.format(player_two), headers=headers).json()['lifeTimeStats']
-			player_two_stats = populate_player_data(player_two_result)
+        player_one_result = requests.get(URL.format(player_one),
+                headers=headers).json()['lifeTimeStats']
+        player_one_stats = populate_player_data(player_one_result)
 
-	return render_template('index.html', player_one=player_one, player_two=player_two,
-		player_one_stats=player_one_stats, player_two_stats=player_two_stats)
+        if player_two:
+            player_two_result = requests.get(URL.format(player_two),
+                    headers=headers).json()['lifeTimeStats']
+            player_two_stats = populate_player_data(player_two_result)
+
+    return render_template('index.html', player_one=player_one,
+                           player_two=player_two,
+                           player_one_stats=player_one_stats,
+                           player_two_stats=player_two_stats)
+
 
 def populate_player_data(api_data):
-	
-	temporary_dict = {}
 
-	for r in api_data:
-		if r['key'] == 'Wins':
-			temporary_dict['wins'] = r['value']
-		if r['key'] == 'Kills':
-			temporary_dict['kills'] = r['value']
-		if r['key'] == 'Matches Played':
-			temporary_dict['matches played'] = r['value']
-		if r['key'] == 'K/d':
-			temporary_dict['k/d'] = r['value']
-		if r['key'] == 'Win%':
-			temporary_dict['win rate'] = r['value']
+    temporary_dict = {}
 
-	return temporary_dict
+    for r in api_data:
+        if r['key'] == 'Wins':
+            temporary_dict['wins'] = r['value']
+        if r['key'] == 'Kills':
+            temporary_dict['kills'] = r['value']
+        if r['key'] == 'Matches Played':
+            temporary_dict['matches played'] = r['value']
+        if r['key'] == 'K/d':
+            temporary_dict['k/d'] = r['value']
+        if r['key'] == 'Win%':
+            temporary_dict['win rate'] = r['value']
 
-#print(populate_player_data(result))
+    return temporary_dict
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
+
+			
